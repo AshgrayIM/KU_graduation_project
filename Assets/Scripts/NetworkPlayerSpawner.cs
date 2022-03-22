f2b73cf58764b88215dabd581fe4a333aa61dc64
ASHGRAY-DK\imcha <imchangkyu@gmail.com>
2022-03-22 13:55:53 +0900

    네트워크 - Transform 통신 구현

---
+++ Assets/Scripts/NetworkPlayerSpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
+{
+    private GameObject spawnedPlayerPrefab;
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
+    }
+
+    public override void OnLeftRoom()
+    {
+        Debug.Log("A player left a Room");
+        base.OnLeftRoom();
+        PhotonNetwork.Destroy(spawnedPlayerPrefab);
+    }
+}

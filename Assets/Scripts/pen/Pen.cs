7459deb9aff8734a30f55d292ecf889ad83acead
suhan.song <hoxz12012@gmail.com>
2022-06-06 05:42:17 +0900

    펜 기능 프로토타이핑 완료

--- Assets/Scripts/pen/Pen.cs
+++ Assets/Scripts/pen/Pen.cs
@@ -6,22 +6,54 @@ using UnityEngine;
 public class Pen : MonoBehaviour
 {
     public GameObject nip;
+    public Material material;
+    public TrailRenderer trailRenderer;
+    public GameObject lineRenderer;
     private bool usingPen = false;
+    private List<Vector3> vectorData;
+    
+    private int inkNo = 0;
+    private string inkPrefix = "ink";
+
+    public void Awake()
+    {
+        material.SetFloat("_Width", 0.005f);
+        trailRenderer.material = material;
+        
+        var lr = lineRenderer.GetComponent<LineRenderer>();
+        lr.material = material;
+    }
+
     public void DrawEnter()
     {
         usingPen = true;
+        trailRenderer.gameObject.SetActive(true);
     }
 
     public void DrawExit()
     {
         usingPen = false;
+        trailRenderer.gameObject.SetActive(false);
+
+        int positionCount = trailRenderer.positionCount;
+        Vector3[] list = new Vector3[positionCount];
+        trailRenderer.GetPositions(list);
+        
+        trailRenderer.Clear();
+        
+        var lineObj = Instantiate(lineRenderer);
+        lineObj.name = $"{inkPrefix} ({inkNo++})";
+        var line = lineObj.GetComponent<LineRenderer>();
+        line.positionCount = positionCount;
+        line.SetPositions(list);
+        line.gameObject.SetActive(true);
     }
 
     public void Update()
     {
         if (usingPen)
         {
-            Debug.LogError(nip.transform.position);
+            vectorData.Add(nip.transform.position);
         }
     }
 }

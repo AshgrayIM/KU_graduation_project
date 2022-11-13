bad59f18818f4d614ee03dc33f760a4def550dfd
suhan.song <hoxz12012@gmail.com>
2022-11-13 20:10:31 +0900

    network delete all

--- Assets/Scripts/pen/InkManager.cs
+++ Assets/Scripts/pen/InkManager.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class InkManager : MonoBehaviour
 {
+    private PhotonView photonView;
 
+    public void Awake()
+    {    
+        photonView = PhotonView.Get(this);
+    }
+    
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Delete))
         {
-            var inkList = GameObject.FindGameObjectsWithTag("Ink");
-            for (int i = 0; i < inkList.Length; i++)
-            {
-                Destroy(inkList[i]);
-            }
+            photonView.RPC("DeleteAll", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    void DeleteAll()
+    {
+        var inkList = GameObject.FindGameObjectsWithTag("Ink");
+        for (int i = 0; i < inkList.Length; i++)
+        {
+            Destroy(inkList[i]);
         }
     }
 }
